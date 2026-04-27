fb510d108b098f9f9a5117b6b622ac3bb9921d72
yejoo0519 <yejoo060519@gmail.com>
2026-04-27 16:43:00 +0900

    Add files via upload

---
+++ data2/guild1.js
@@ -0,0 +1,29 @@
+const guildContent = {
+  id: 1,
+  title: "Genesis",
+  level: "Lv.26",
+  date: "2026.04.25",
+  logo: "https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/guild/1.png",
+  interview: "https://open.kakao.com/o/gV9c5J0g",
+  body: `
+    <div class="section-title">길드 소개</div>
+    <p>26렙 길드 제네시스에서 함께하실 길드원분을 모집합니다.</p>
+    <p>저희 "Genesis"에서 함께, 오래, 즐겁게 게임하실 분들을 찾고 있습니다.</p>
+
+    <hr>
+
+    <div class="section-title">가입 조건</div>
+    <p>• 길드전 사용 가능한 정령기준 6풀정 이상</p>
+    <p>• 정령에 대한 이해도가 높으신 분</p>
+    <p>• 길드전 꾸준히 참여 가능하신 분</p>
+    <p>• 18세 이상</p>
+    <p>• 요르문간드 클리어 가능하신 분 우대</p>
+
+    <hr>
+
+    <div class="section-title">세부 사항</div>
+    <p>• 길톡방 필수, 닉변 자유, 광산 자유, 길포 자유</p>
+    <p>• 기본적인 예의가 갖춰져 있으며 논란이나 사건이 없는 분</p>
+    <p>• 꾸준히 열심히 하며 성장 가능성이 있으신 분 모두 환영합니다.</p>
+  `
+};
